a8d4603b7088b66022a946eae2d0227c422217f9
omaralalwi <o.alalwi@iajeer.com>
2025-01-25 02:36:43 +0300

    bugfix with enum

--- src/Enums/Configs/TemperatureValues.php
+++ src/Enums/Configs/TemperatureValues.php
@@ -7,7 +7,7 @@ enum TemperatureValues: string
     case CODING = "0.0";
     case MATH = "0.1";
     case DATA_ANALYSIS = "1.0";
-    case DATA_CLEANING = "1.1"; // Fixed missing closing quote
+    case DATA_CLEANING = "1.1";g
     case GENERAL_CONVERSATION = "1.3";
     case TRANSLATION = "1.4";
     case CREATIVE_WRITING = "1.5";
--- src/Enums/Models.php
+++ src/Enums/Models.php
@@ -7,5 +7,5 @@ enum Models: string
     case CHAT = 'deepseek-chat';
     case CODER = 'deepseek-coder';
     case R1 = 'DeepSeek-R1';
-    case R1-Zero = 'DeepSeek-R1-Zero';
+    case R1Zero = 'DeepSeek-R1-Zero';
 }
